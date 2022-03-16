8d182e83af302cfe27779440af9564492d7589fe
PlagueMasque <aknyldrm361@gmail.com>
2022-03-16 12:15:23 +0300

    GameManager

--- CoupleShuffle/Assets/Scripts/GameManager.cs
+++ CoupleShuffle/Assets/Scripts/GameManager.cs
@@ -5,16 +5,58 @@ public class GameManager : MonoBehaviour
     [SerializeField] private PlayerMoney leftPlayer;
 
     [SerializeField] private PlayerMoney rightPlayer;
-    
+
+    public bool canRaiseRight;
+    public bool canRaiseLeft;
+
+    private float timer;
+
+    private float startTimer = 0.1f;
+
+    private void Update() 
+    {
+        if(timer <= 0)
+        {
+            if(canRaiseRight)
+                RaiseRight();
+
+            if(canRaiseLeft)
+                RaiseLeft();
+
+            timer = startTimer;
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+
     public void RaiseRight()
     {
-        rightPlayer.Money++;
-        leftPlayer.Money--;
+        if(leftPlayer.Money > 0)
+        {   
+            rightPlayer.Money++;
+            leftPlayer.Money--;   
+        }
+        
     }
     public void RaiseLeft()
     {
-        leftPlayer.Money++;
-        rightPlayer.Money--;
+        if(rightPlayer.Money > 0)
+        {
+            leftPlayer.Money++;
+            rightPlayer.Money--;
+        }
+    }
+
+    public void CanRaiseRight(bool raise)
+    {
+        canRaiseRight = raise;
+    }
+
+    public void CanRaiseLeft(bool raise)
+    {
+        canRaiseLeft = raise;
     }
 
 }
